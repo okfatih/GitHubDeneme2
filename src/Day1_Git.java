6f4505cc60883c35a57c0b45c835c58b38343730
Fatih <fatih_ok4@hotmail.com>
2022-08-14 20:35:50 +0200

    V1

---
+++ src/Day1_Git.java
@@ -0,0 +1,6 @@
+public class Day1_Git {
+    public static void main(String[] args) {
+        System.out.println("Github git first trial");
+        System.out.println("Github git second trial");
+    }
+}
